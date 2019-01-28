fc46cad10d3914d9c2fd5df24a28d9e668ae4625
Skullbonk <reagan.fairbanks@gmail.com>
2019-01-28 12:20:36 -0700

    Initial commit

---
+++ DataBoyoTime/DeadDataStructure.cpp
@@ -0,0 +1,9 @@
+//
+//  DeadDataStructure.cpp
+//  DataBoyoTime
+//
+//  Created by Fairbanks, Reagan on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "DeadDataStructure.hpp"
---
+++ DataBoyoTime/DeadDataStructure.hpp
@@ -0,0 +1,14 @@
+//
+//  DeadDataStructure.hpp
+//  DataBoyoTime
+//
+//  Created by Fairbanks, Reagan on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef DeadDataStructure_hpp
+#define DeadDataStructure_hpp
+
+#include <stdio.h>
+
+#endif /* DeadDataStructure_hpp */
