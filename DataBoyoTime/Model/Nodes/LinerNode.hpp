8c5228420ef392164c0bb548f55cb34c996708ef
Skullbonk <reagan.fairbanks@gmail.com>
2019-01-30 13:45:57 -0700

    ehk

--- DataBoyoTime/Model/Nodes/LinerNode.hpp
+++ DataBoyoTime/Model/Nodes/LinerNode.hpp
@@ -6,8 +6,39 @@
 //  Copyright © 2019 CTEC. All rights reserved.
 //
 
-#ifndef LinerNode_h
-#define LinerNode_h
+#ifndef LinerNode_hpp
+#define LinerNode_hpp
+
+#include "Node.hpp"
+
+template <class Type>
+class LinearNode : public Node<Type>
+{
+private:
+	LinearNode : public Node<Type>
+
+private:
+	LinearNode<Type> * next;
+public:
+	LinearNode();
+	LinearNode(Type data);
+	LinearNode(Type data, LinearNode<Type> * next);
+	LinearNode<Type> * getNext();
+	void setNext(LinearNode<Type> * next);
+};
+
+//Implementing templates
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node()
+{
+	
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data) : Node<Type>(data)
+{
+	this->next = nullptr;
+}
 
 
-#endif /* LinerNode_h */
+#endif /* LinerNode_hpp */
